275f9f838c562751e69217d202ee0db832ac4e0a
Nicolas Corbalan <outreef@hotmail.com>
2021-07-29 00:56:29 +0200

    test1

--- src/Hello.js
+++ src/Hello.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Hello(){
   return (
-    <div>Hello</div>
+    <div>
+      <h1>Hello</h1>
+    </div>
   )
 }
 
--- src/Hello.test.js
+++ src/Hello.test.js
@@ -3,5 +3,8 @@ import Hello from './Hello';
 import {render} from '@testing-library/react';
 
 test("Component should display Hello text", () => {
-  render(<Hello />)
+  const comp = render(<Hello />);
+  //comp.debug();
+  const helloText = comp.getByText("Hello");
+  expect(helloText).toBeTruthy();
 });
